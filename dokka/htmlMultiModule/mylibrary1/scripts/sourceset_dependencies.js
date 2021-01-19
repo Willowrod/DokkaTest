5615a3ade89a1fe6cffd9e1c8428193af6002e17
Mike Hall <mike@willowrod.com>
2021-01-19 12:04:07 +0000

    Implemented Dokka MultiModule

---
+++ dokka/htmlMultiModule/mylibrary1/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":mylibrary1:dokkaHtml/androidTestRelease": [], ":mylibrary1:dokkaHtml/debug": [], ":mylibrary1:dokkaHtml/main": [], ":mylibrary1:dokkaHtml/release": []}'
---
+++ dokka/htmlMultiModule/mylibrary2/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":mylibrary2:dokkaHtml/androidTestRelease": [], ":mylibrary2:dokkaHtml/debug": [], ":mylibrary2:dokkaHtml/main": [], ":mylibrary2:dokkaHtml/release": []}'
