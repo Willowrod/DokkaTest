aa4d118cc9a23746adc2922c9d456a879564869b
Mike Hall <mike@willowrod.com>
2021-01-19 10:49:55 +0000

    Initial commit

---
+++ mylibrary1/src/main/java/com/willowrod/mylibrary1/Lib1.kt
@@ -0,0 +1,16 @@
+package com.willowrod.mylibrary1
+
+import android.util.Log
+
+/**
+ * Library 1
+ */
+class Lib1 {
+
+    /**
+     * Logs message to console testing library 1
+     */
+    fun testLib1(){
+        Log.d("WRD", "Tests Library 1")
+    }
+}
---
+++ mylibrary2/src/main/java/com/willowrod/mylibrary2/Lib2.kt
@@ -0,0 +1,16 @@
+package com.willowrod.mylibrary2
+
+import android.util.Log
+
+/**
+ * Library 2
+ */
+class Lib2 {
+
+    /**
+     * Logs message to console testing library 2
+     */
+    fun testLib2(){
+        Log.d("WRD", "Tests Library 2")
+    }
+}
